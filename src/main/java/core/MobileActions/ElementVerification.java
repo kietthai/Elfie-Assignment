3702344efd9e796a0c32e48a4266d78a1125cb50
Admin <thai.kiet520@gmail.com>
2025-03-27 21:21:19 +0700

    Enhance Framework (6)

--- src/main/java/core/MobileActions/ElementVerification.java
+++ src/main/java/core/MobileActions/ElementVerification.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.WebElement;
 
 public class ElementVerification {
 
-    public static boolean isElementDisplayed(ElementType type, String value, int... timeout) {
+    public static boolean isElementDisplayed(ElementType type, String value, long timeout) {
         try {
-            WebElement element = WaitActions.waitForElementVisible(type, value);
+            WebElement element = WaitActions.waitForElementVisible(type, value, timeout);
             boolean displayed = element.isDisplayed();
             Logger.logInfo("Element [" + value + "] is displayed: " + displayed);
             return displayed;
@@ -18,7 +18,7 @@ public class ElementVerification {
         }
     }
 
-    public static boolean isElementEnabled(ElementType type, String value, int... timeout) {
+    public static boolean isElementEnabled(ElementType type, String value, long timeout) {
         try {
             WebElement element = WaitActions.waitForElementVisible(type, value, timeout);
             boolean enabled = element.isEnabled();
@@ -30,9 +30,45 @@ public class ElementVerification {
         }
     }
 
-    public static boolean isElementSelected(ElementType type, String value, int... timeout) {
+    public static boolean isElementSelected(ElementType type, String value, long timeout) {
         try {
             WebElement element = WaitActions.waitForElementVisible(type, value, timeout);
+            boolean selected = element.isSelected();
+            Logger.logInfo("Element [" + value + "] is selected: " + selected);
+            return selected;
+        } catch (Exception e) {
+            Logger.logInfo("Element [" + value + "] not found.");
+            return false;
+        }
+    }
+
+    public static boolean isElementDisplayed(ElementType type, String value) {
+        try {
+            WebElement element = WaitActions.waitForElementVisible(type, value);
+            boolean displayed = element.isDisplayed();
+            Logger.logInfo("Element [" + value + "] is displayed: " + displayed);
+            return displayed;
+        } catch (Exception e) {
+            Logger.logInfo("Element [" + value + "] not found.");
+            return false;
+        }
+    }
+
+    public static boolean isElementEnabled(ElementType type, String value) {
+        try {
+            WebElement element = WaitActions.waitForElementVisible(type, value);
+            boolean enabled = element.isEnabled();
+            Logger.logInfo("Element [" + value + "] is enabled: " + enabled);
+            return enabled;
+        } catch (Exception e) {
+            Logger.logInfo("Element [" + value + "] not found.");
+            return false;
+        }
+    }
+
+    public static boolean isElementSelected(ElementType type, String value) {
+        try {
+            WebElement element = WaitActions.waitForElementVisible(type, value);
             boolean selected = element.isSelected();
             Logger.logInfo("Element [" + value + "] is selected: " + selected);
             return selected;
--- src/main/java/core/MobileActions/WaitActions.java
+++ src/main/java/core/MobileActions/WaitActions.java
@@ -12,15 +12,27 @@ import java.time.Duration;
 import static config.contants.Timeouts.IMPLICIT_WAIT;
 
 public class WaitActions {
-    private static WebDriverWait getWait(int... timeouts) {
-        return new WebDriverWait(DriverManager.getMobileDriver(), Duration.ofSeconds(IMPLICIT_WAIT));
+    private static WebDriverWait getWait(long timeouts) {
+        return new WebDriverWait(DriverManager.getMobileDriver(), Duration.ofSeconds(timeouts));
     }
 
-    public static WebElement waitForElementVisible(ElementType type, String value, int... timeouts) {
+    private static WebDriverWait getWait() {
+        return getWait(IMPLICIT_WAIT);
+    }
+
+    public static WebElement waitForElementVisible(ElementType type, String value, long timeouts) {
         return getWait(timeouts).until(ExpectedConditions.visibilityOfElementLocated(ElementLocator.getBy(type, value)));
     }
 
-    public static WebElement waitForElementToBeClickable(ElementType type, String value, int... timeouts) {
+    public static WebElement waitForElementToBeClickable(ElementType type, String value, int timeouts) {
         return getWait(timeouts).until(ExpectedConditions.elementToBeClickable(ElementLocator.getBy(type, value)));
+    }
+
+    public static WebElement waitForElementVisible(ElementType type, String value) {
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(ElementLocator.getBy(type, value)));
+    }
+
+    public static WebElement waitForElementToBeClickable(ElementType type, String value) {
+        return getWait().until(ExpectedConditions.elementToBeClickable(ElementLocator.getBy(type, value)));
     }
 }
